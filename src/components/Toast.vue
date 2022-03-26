7a5442a0d9359e5a9dcd8a50786d2160d23d6982
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-03-26 10:27:11 +0530

    🎨 stack toast behind and reverse order

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="toasts?.length"  class="flex justify-center md:justify-end">
-    <div class="fixed bottom-0">
+    <div class="fixed bottom-0 stack">
       <transition-group name="list">
-        <div v-for="toast in toasts" :key="toast.id" class="m-2">
+        <div v-for="toast in reverseToast(toasts)" :key="toast.id" class="m-2">
           <div :class="{'alert shadow-lg text-white':true, 'alert-error': toast.type==='error',
           'alert-success': toast.type==='success', 'alert-warning': toast.type==='warning'}">
             <div><span>{{ toast.message }}</span></div>
@@ -20,12 +20,18 @@
 <script setup lang="ts">
   import _ from "lodash";
   import { getToasts } from "@/composables/toasts";
+  import { ToastData } from "@/assets/js/types";
   
   let toasts = getToasts();
   const removeToast = (id: number) =>{
     const isOnIndex = (_.findIndex(toasts.value, {id: id}));
     toasts.value.splice(isOnIndex, 1);
   };
+
+  const reverseToast = (toasts: ToastData[]) => {
+    return [...toasts].reverse()
+  }
+
 </script>
 
 
