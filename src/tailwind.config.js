0f4b76c762722b95f289611aacb670bafa0adb57
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2023-01-08 19:42:22 +0530

    ⬆️ upgrade to nuxt3

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
