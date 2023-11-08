39a461070bef4d42544f0114a2a32cd2df507bb2
Akhil Narayanan <akhil.narayanan@se.com>
2023-11-08 20:20:32 +0530

    ♻️ refactor: completeprofile as component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="navbar bg-base-100">
+        <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </button>
+        </div>
+        <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl">ConfessTo.Me</a>
+        </div>
+        <div class="flex-none">
+            <button class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                    <span class="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+            </button>
+            <div class="dropdown dropdown-end">
+                <button class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                </button>
+                <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                    <li><a>Edit Profile</a></li> 
+                    <li onclick="logout_confirm.showModal()"><a>LogOut</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <dialog id="logout_confirm" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+            <h3 class="font-bold text-2xl">Sure?</h3>
+            <p class="mt-6">Are you sure, you want to log-out?</p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button @click="signOutUser" class="btn btn-ghost">CONFIRM</button>
+                    <button class="btn">CLOSE</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
+    
+</template>
+
+<script setup lang="ts">
+    import { signOut } from "firebase/auth";
+    const signOutUser = async () => {
+        const auth = useFirebaseAuth()!;
+        const user = firebaseUser().value || await getUserDataPromised();
+        if (!user.isAnonymous) {
+            signOut(auth).catch(error => {
+                addToast({
+                    message: error,
+                    type: "error",
+                });
+            });
+        } else {
+            addToast({
+                message: "Already signedout. Can't signout again.",
+                type: "error",
+                duration: 3000,
+            });
+        };
+    };
+</script>
