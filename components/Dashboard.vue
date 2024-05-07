89b9ee394657bc215f1dfa89a1edc16ecefb8dff
Akhil Narayanan <akhil.narayanan@se.com>
2024-05-07 11:49:00 +0530

    🐛 fix: theme change on localstorage

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-    <div v-if="!profile.username">
-        <LoadingNavbar />
+    <Navbar :profile="props.profile"/>
+    <div v-if="!props.profile.username">
         <div class="flex justify-center mx-4">
             <div class="skelton h-16 max-w-96 ">
                 <span class="text-base-100">a</span>
@@ -9,10 +9,9 @@
         </div>
     </div>
     <div v-else>
-        <Navbar />
         <div class="flex justify-center mx-4">
             <div class="alert bg-emerald-600 max-w-96 shadow-lg flex justify-between">
-                <span class="text-base-100">https://confessto.me/@{{ profile.username }}</span>
+                <span class="text-base-100">https://confessto.me/@{{ props.profile.username }}</span>
                 <div class="flex space-x-2">
                 <button class="btn btn-ghost bg-emerald-700 min-h-7 h-7 px-1">
                     <span class="material-symbols-outlined text-lg font-light text-base-100">content_copy</span>
@@ -46,9 +45,9 @@
 </template>
 
 <script setup lang="ts">
+    import type { FirestoreUserProfile } from "@/assets/js/types";
+
     const props = defineProps<{
-        profile: {
-            username: string;
-        };
+        profile: FirestoreUserProfile;
     }>();
 </script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,26 +8,22 @@
         <div class="flex-1">
             <a class="btn btn-ghost normal-case text-xl">ConfessTo.Me</a>
         </div>
-        <div class="dropdown">
-            <div tabindex="0" role="button" class="btn">Theme</div>
-            <ul tabindex="0" class="dropdown-content z-[1] shadow-2xl menu bg-base-200 rounded-box">
-                <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Light" value="light"/></li>
-                <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dark" value="dark"/></li>
-                <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" value="default"/></li>
-            </ul>
-        </div>
         <div class="flex-none">
-            <!-- <button class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                    <span class="badge badge-xs badge-primary indicator-item"></span>
-                </div>
-            </button> -->
+            <label class="swap swap-rotate">
+                <input data-key="theme" data-toggle-theme="light,dark" data-act-class="ACTIVECLASS" type="checkbox" class="theme-controller" />                    
+                <span :class="{'fill-current material-symbols-outlined': true, 'swap-on': isDark, 'swap-off': !isDark}">dark_mode</span>
+                <span :class="{'fill-current material-symbols-outlined': true, 'swap-off': isDark, 'swap-on': !isDark}">light_mode</span>
+            </label>
             <div class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="btn btn-ghost">
                     <span class="material-symbols-outlined">more_vert</span>
                 </div>
-                <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                <ul v-if="!props.profile.username" tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                    <div v-for="_ in Array.from({length: 5})">
+                        <li><a class="skeleton h-3 w-14 my-2"></a></li> 
+                    </div>
+                </ul>
+                <ul v-else tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                     <li><a>Edit Profile</a></li> 
                     <li onclick="logout_confirm.showModal()"><a>Logout</a></li>
                 </ul>
@@ -52,10 +48,18 @@
 </template>
 
 <script setup lang="ts">
-    import { signOut, unlink, linkWithCredential, signInAnonymously, type User, type AuthCredential } from "firebase/auth";
-    
+    import { themeChange } from "theme-change";
+    import type { FirestoreUserProfile } from "@/assets/js/types";
+
     const auth = useFirebaseAuth()!;
     const user = useCurrentUser();
+
+    const isDark = ref(false);
+
+    onMounted(() => {
+        isDark.value = localStorage.getItem("theme") === "dark" ? true : false;
+        themeChange(false);
+    })
 
     const signOutUser = async () => {
         if (!user.value?.isAnonymous as boolean) {
@@ -83,4 +87,8 @@
             });
         };
     };
+
+    const props = defineProps<{
+        profile: FirestoreUserProfile;
+    }>();
 </script>
